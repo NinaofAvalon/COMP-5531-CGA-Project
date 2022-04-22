50a01964a5609e9216aac4cde3c6861071a7a206
NinaofAvalon <ninaofavalon@gmail.com>
2022-04-21 20:36:48 -0400

    instructor connection changes

--- Instructor/connection.php
+++ Instructor/connection.php
@@ -1,7 +1,7 @@
  <?php
 session_start();
 
-$con = mysqli_connect("localhost","root","root","project");
+$con = mysqli_connect("qtc5531.encs.concordia.ca","qtc55314","rkf3kQ","qtc55314");
 
 if(!$con)
 {
