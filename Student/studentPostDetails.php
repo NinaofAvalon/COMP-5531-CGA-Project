0e63959efab50c4192c4b3b19864847828477a2a
madelson23 <64505503+madelson23@users.noreply.github.com>
2022-04-23 23:26:48 -0400

    Update studentPostDetails.php

--- Student/studentPostDetails.php
+++ Student/studentPostDetails.php
@@ -85,7 +85,7 @@
 
 
     <!-- menu -->
-    <div class="menu" height="100%" width="150px">
+    <div class="menu-welcome" height="100%" width="150px">
       <hr>
       <b >
         <font size="4">
