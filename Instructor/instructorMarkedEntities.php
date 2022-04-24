2742a78a7b10fa7fa69e47e70e3fc3e25e73f21d
madelson23 <64505503+madelson23@users.noreply.github.com>
2022-04-24 00:57:58 -0400

    Update instructorMarkedEntities.php

--- Instructor/instructorMarkedEntities.php
+++ Instructor/instructorMarkedEntities.php
@@ -54,7 +54,7 @@ include('../session.php');
         </div>
 
         <!-- menu -->
-        <div class="menu" height="100%" width="150px">
+        <div class="menu-welcome" height="100%" width="150px">
             <hr>
             <b>
                 <font size="4">
